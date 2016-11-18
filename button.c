b0bda9a076ebb432829b64132152a26c4a9a5966
Jake's Raspberry Pi <runyanjake@gmail.com>
2016-11-18 05:04:38 +0000

    Light show 1 and FSM mostly working.

--- button.c
+++ button.c
@@ -12,27 +12,76 @@
 #define LED_8 21
 #define BUT_IN 26
 #define BUT_OUT 19
+#define DELAY_TIME_MS 250
+
+//**** Define Enums ****
+enum{
+	STARTUP,
+	MODE1,
+	MODE2,
+	MODE3,
+	SHUTDOWN
+	} state = STARTUP;
+
+//**** Function Declarations ****
+int lightShowBlink();
 
 int main(){
-	//Setup
-	wiringPiSetupGpio();
-	pinMode(LED_1, OUTPUT);
-	pinMode(LED_2, OUTPUT);
-	pinMode(LED_3, OUTPUT);
-	pinMode(LED_4, OUTPUT);
-	pinMode(LED_5, OUTPUT);
-	pinMode(LED_6, OUTPUT);
-	pinMode(LED_7, OUTPUT);
-	pinMode(LED_8, OUTPUT);
-	pinMode(BUT_OUT, INPUT);
-	pinMode(BUT_IN, OUTPUT);
+	while(1){
+		switch (state){
+			case STARTUP:
+				//Setup
+				wiringPiSetupGpio();
+				pinMode(LED_1, OUTPUT);
+				pinMode(LED_2, OUTPUT);
+				pinMode(LED_3, OUTPUT);
+				pinMode(LED_4, OUTPUT);
+				pinMode(LED_5, OUTPUT);
+				pinMode(LED_6, OUTPUT);
+				pinMode(LED_7, OUTPUT);
+				pinMode(LED_8, OUTPUT);
+				pinMode(BUT_OUT, INPUT);
+				pinMode(BUT_IN, OUTPUT);
+				//Pin 19 (Button input) always constant
+				digitalWrite(BUT_IN, 1);
+				state = MODE1;
+			case MODE1:
+				if(lightShowBlink() == 1){
+					state = MODE2;
+				}
+				break;
+			case MODE2:
+				if(1 == 1){
+					state = MODE3;
+				}
+				break;
+			case MODE3:
+				if(1 == 1){
+					state = SHUTDOWN;
+				}
+				break;
+			case SHUTDOWN:
+				digitalWrite(LED_1, LOW);
+		                digitalWrite(LED_2, LOW);
+	         	        digitalWrite(LED_3, LOW);
+	         	        digitalWrite(LED_4, LOW);
+	         	        digitalWrite(LED_5, LOW);
+	         	        digitalWrite(LED_6, LOW);
+	         	        digitalWrite(LED_7, LOW);
+	         	        digitalWrite(LED_8, LOW);
+				digitalWrite(BUT_IN, 0);
+				return 1; //ends program
+		}
+	}
+}
 
-	//Pin 19 (Button input) always constant
-	digitalWrite(BUT_IN, 1);
-
-	//Loop
+//Blink Loop
+int lightShowBlink(){
 	int ctrl = 0;
 	for(ctrl=0;ctrl<10;ctrl++){
+		//check for button press often enough so that it's responsive
+		if(digitalRead(BUT_OUT))return 1;
+
 		digitalWrite(LED_1, HIGH);
 		digitalWrite(LED_2, HIGH);
 		digitalWrite(LED_3, HIGH);
@@ -43,7 +92,10 @@ int main(){
 		digitalWrite(LED_8, HIGH);
 		printf("Button: %d\n", digitalRead(BUT_OUT));
 
-		delay(1000);
+		delay(DELAY_TIME_MS);
+
+		//check for button press often enough so that it's responsive
+                if(digitalRead(BUT_OUT))return 1;
 
 		digitalWrite(LED_1, LOW);
 		digitalWrite(LED_2, LOW);
@@ -55,11 +107,9 @@ int main(){
 		digitalWrite(LED_8, LOW);
 		printf("Button: %d\n", digitalRead(BUT_OUT));
 
-		delay(1000);
+		delay(DELAY_TIME_MS);
 	}
-
-	//End Cleanup
-	digitalWrite(BUT_IN, 0);
-	return 1;
+	return 0;
 }
+
 
